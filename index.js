d268a53d17f111cb7135f98d5429d6b801893de5
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2023-12-20 23:33:02 +0530

    created a working POSTmethod when connected with db

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
+const urlRoute = require("./routes/url");
+const {connectToMongoose} = require("./connect");
 const app = express();
 const PORT = 4000;
+
+const uri = "mongodb://127.0.0.1:27017/short-url"
+connectToMongoose(uri)
+.then(() => console.log("Mongodb connected"))
+
+app.use(express.json());
+
+app.use("/url",urlRoute);
 
 app.listen(PORT, () => console.log("Server started at port ", PORT));
---
+++ models/url.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+    shortId: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    redirectUrl: {
+        require: true,
+        type: String,
+    },
+    visitHistory: [{ timestamp: {type: Number}}],
+}, 
+    {timestamps: true}
+);
+
+const URL = mongoose.model("url", urlSchema);
+module.exports = URL;
