5108ad2be8333387c528ecf603355b0ebb616c2c
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2023-12-22 19:37:29 +0530

    server side rendered using EJS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const path = require("path")
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter")
 const URL = require("./models/url");
 const { connectToMongoose } = require("./connect");
 const app = express();
@@ -8,11 +10,16 @@ const PORT = 4000;
 const uri = "mongodb://127.0.0.1:27017/short-url";
 connectToMongoose(uri).then(() => console.log("Mongodb connected"));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.use("/url", urlRoute);
+app.use("/",staticRoute);
 
-app.get("/:shortid", async (req, res) => {
+app.get("/url/:shortid", async (req, res) => {
   const shortId = req.params.shortid;
   const entry = await URL.findOneAndUpdate(
     {
